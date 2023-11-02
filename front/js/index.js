77595960266b1093da81b0bfa70e96868f70c96a
1juba <112666824+1juba@users.noreply.github.com>
2023-11-02 19:57:55 -0300

    Creacion index.js
    Creacion index.js

---
+++ front/js/index.js
@@ -0,0 +1,83 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+
+/** Crea las tarjetas de productos teniendo en cuenta la lista en cervezas.js */
+function crearTarjetasProductosInicio(productos){
+  contenedorTarjetas.innerHTML = '';
+  productos.forEach(producto => {
+    const nuevaCerveza = document.createElement("div");
+    nuevaCerveza.classList = "tarjeta-producto"
+    nuevaCerveza.category  = producto.category
+    nuevaCerveza.innerHTML = `
+    <img src="${producto.imagen}">
+    <h3>${producto.titulo}</h3>
+    <p class="precio">$${producto.precio}</p>
+    <button>Agregar al carrito</button>`
+    contenedorTarjetas.appendChild(nuevaCerveza);
+    nuevaCerveza.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+  });
+}
+
+
+
+// Filtrar productos por categoría
+function filtrarProductosPorCategoria(categoria) {
+  const tarjetasProductos = document.querySelectorAll(".tarjeta-producto");
+  tarjetasProductos.forEach(tarjeta => {
+    if (tarjeta.category === categoria) {
+      tarjeta.style.display = "block";
+    } else {
+      tarjeta.style.display = "none";
+    }
+  });
+}
+
+// Obtén la lista completa de cervezas y crea las tarjetas de productos iniciales
+getCervezas().then(cervezas => {
+  crearTarjetasProductosInicio(cervezas);
+
+  // Agregar eventos de clic a botones de filtro (por ejemplo, categoría "Andes")
+  document.getElementById("andes").addEventListener("click", () => {
+    filtrarProductosPorCategoria("Andes");
+  });
+
+  document.getElementById("quilmes").addEventListener("click", () => {
+    filtrarProductosPorCategoria("Quilmes");
+  });
+
+  document.getElementById("imperial").addEventListener("click", () => {
+    filtrarProductosPorCategoria("Imperial");
+  });
+
+  document.getElementById("schneider").addEventListener("click", () => {
+    filtrarProductosPorCategoria("Schneider");
+  });
+
+  document.getElementById("stella").addEventListener("click", () => {
+    filtrarProductosPorCategoria("Stella");
+  });
+
+  document.getElementById("estrella").addEventListener("click", () => {
+    filtrarProductosPorCategoria("EstrellaGalicia");
+  });
+
+  document.getElementById("corona").addEventListener("click", () => {
+    filtrarProductosPorCategoria("Corona");
+  });
+
+  document.getElementById("heineken").addEventListener("click", () => {
+    filtrarProductosPorCategoria("Heineken");
+  });
+
+  // Agregar evento de clic al botón "Todos"
+  document.getElementById("todos").addEventListener("click", () => {
+    mostrarTodosLosProductos(cervezas);
+  });
+  
+  
+});
+
+// Función para mostrar todos los productos sin filtro
+function mostrarTodosLosProductos(cervezas) {
+  // Llama a la función para crear tarjetas de productos con la lista completa de cervezas
+  crearTarjetasProductosInicio(cervezas);
+}
