54675e332881e05425b86632250982b95533eb07
Jorge-Agustin-Loyola <112104103+Jorge-Agustin-Loyola@users.noreply.github.com>
2023-11-06 20:40:56 -0300

    agerego cartService y indexmercado

---
+++ front/js/indexmercado.js
@@ -0,0 +1,50 @@
+const mercadopago = new MercadoPago("TEST-7a98890d-5eb1-48bd-b3d5-31a9e6751d8a", {
+  locale: "es-AR", // The most common are: 'pt-BR', 'es-AR' and 'en-US'
+});
+
+document.getElementById("checkout-btn").addEventListener("click", function () {
+  const orderData = {
+    quantity: 1, 
+    price: document.getElementById("unit-price").innerHTML,
+  };
+
+  fetch("http://localhost:8080/create_preference", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(orderData),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (preference) {
+      createCheckoutButton(preference.id);
+    })
+    .catch(function () {
+      alert("Unexpected error");
+    });
+});
+
+function createCheckoutButton(preferenceId) {
+  // Initialize the checkout
+  const bricksBuilder = mercadopago.bricks();
+
+  const renderComponent = async (bricksBuilder) => {
+    if (window.checkoutButton) window.checkoutButton.unmount();
+    await bricksBuilder.create(
+      "wallet",
+      "button-checkout", // class/id where the payment button will be displayed
+      {
+        initialization: {
+          preferenceId: preferenceId,
+        },
+        callbacks: {
+          onError: (error) => console.error(error),
+          onReady: () => {},
+        },
+      }
+    );
+  };
+  window.checkoutButton = renderComponent(bricksBuilder);
+}
