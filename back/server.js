6e20a020052d9dbd3887564d3af06f91ba4bd167
MatiasMichaux <112663570+MatiasMichaux98@users.noreply.github.com>
2023-11-07 18:06:21 -0300

    comentando codigo
    comentando codigo

--- back/server.js
+++ back/server.js
@@ -1,33 +1,37 @@
+//importacion de las librerias necesarias a utilizar
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
 const mercadopago = require("mercadopago");
-const database = require("./src/database");
+const database = require("./src/database");// Importación de un módulo personalizado para la base de datos
 
 //middlewares
+// Configuración de CORS para permitir ciertos orígenes
 app.use(cors({
   origin:["http://127.0.0.7:5501","http://127.0.0.7:5500"]
 }))
+// Registro de solicitudes HTTP con Morgan
 app.use(morgan("dev"));
 
+// Análisis de datos JSON y URL codificada
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+// Configuración de rutas estáticas para servir archivos desde una carpeta "front"
 app.use(express.static(path.join(__dirname, "../front")));
 app.use(cors());
 
-// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+// REEMPLAZAMOS CON TU TOKEN DE ACCESO DISPONIBLE EN: https://developers.mercadopago.com/panel
 mercadopago.configure({
   access_token: "TEST-5397765485790054-102111-48ef2db9170a74cb10c8f1b1e1055206-742117903",
 });
-
+// Ruta raíz que envía un archivo HTML
 app.get("/", function (req, res) {
   const filePath = path.resolve(__dirname, "../front", "cart.html");
   res.sendFile(filePath);
 });
-
+// Ruta para crear una preferencia de pago con MercadoPago
 app.post("/create_preference", (req, res) => {
   let preference = {
     items: [
@@ -44,7 +48,7 @@ app.post("/create_preference", (req, res) => {
     },
     auto_return: "approved",
   };
-
+// Crear la preferencia
   mercadopago.preferences
     .create(preference)
     .then(function (response) {
@@ -56,7 +60,7 @@ app.post("/create_preference", (req, res) => {
       console.log(error);
     });
 });
-
+// Ruta para recibir información de confirmación de pago desde MercadoPago
 app.get("/feedback", function (req, res) {
   res.json({
     Payment: req.query.payment_id,
@@ -64,12 +68,12 @@ app.get("/feedback", function (req, res) {
     MerchantOrder: req.query.merchant_order_id,
   });
 });
-
+// Configuramos del puerto y el inicio del servidor
 app.set("port",8080);
 app.listen(app.get("port"));
 console.log("escuchando el puerto "+app.get("port"));
 
-
+// Ruta para obtener datos de productos desde una base de datos
 app.get("/productos", async (req, res) =>{
   try {
     const connection = await database.getConnection();
--- back/src/database.js
+++ back/src/database.js
@@ -1,7 +1,10 @@
+//importacion de las librerias necesarias a utilizar
 const mysql = require("promise-mysql");
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config();// Cargar las variables de entorno definidas en un archivo .env en el entorno Node.js
+// Esto permite acceder a estas variables a través de process.env en el programa.
 
+// Se crea una conexión a la base de datos utilizando variables de entorno
 const connection = mysql.createConnection({
     host: process.env.host,
     database: process.env.database,
@@ -9,8 +12,9 @@ const connection = mysql.createConnection({
     password: process.env.password
 })
 
+// Se define una función asincrónica para obtener la conexión
 const getConnection = async ()=> await connection;
-
+// Exportamos la función getConnection para su uso en otros módulos
 module.exports ={
     getConnection
 }
