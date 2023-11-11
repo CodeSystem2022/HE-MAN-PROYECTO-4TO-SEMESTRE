d999db0dd0434057ba71976a3fa49c27f055a6da
fabjav <112377555+fabjav@users.noreply.github.com>
2023-11-10 21:36:15 -0300

    cartjs

---
+++ front/js/cart.js
@@ -0,0 +1,81 @@
+/**fabio flores */
+const contenedorTarjetas = document.getElementById("cart-container");
+const cantidadElement = document.getElementById("quantity");
+const precioElement = document.getElementById("unit-price");
+const carritoVacioElement = document.getElementById("carrito-vacio");
+const totalesContainer = document.getElementById("totales");
+
+/** Crea las tarjetas de productos teniendo en cuenta lo guardado en localstorage */
+function crearTarjetasProductosCarrito() {
+  contenedorTarjetas.innerHTML = "";
+  const productos = JSON.parse(localStorage.getItem("cervezas"));
+  if (productos && productos.length > 0) {
+    productos.forEach((producto) => {
+      const nuevaCerveza = document.createElement("div");
+      nuevaCerveza.classList = "tarjeta-producto";
+      nuevaCerveza.innerHTML = `
+    <img src="${producto.imagen}" alt="Cerveza 1">
+    <h3>${producto.titulo}</h3>
+    <span>$${producto.precio}</span>
+    <div>
+    <button>-</button>
+    <span class="cantidad">${producto.cantidad}</span>
+    <button>+</button>
+    </div>
+    `;
+      contenedorTarjetas.appendChild(nuevaCerveza);
+      nuevaCerveza
+        .getElementsByTagName("button")[0]
+        .addEventListener("click", (e) => {
+          const cantidadElement = e.target.parentElement.getElementsByClassName("cantidad")[0];
+          cantidadElement.innerText = restarAlCarrito(producto);
+          crearTarjetasProductosCarrito();
+          actualizarTotales();
+        });
+        nuevaCerveza
+        .getElementsByTagName("button")[1]
+        .addEventListener("click", (e) => {
+          const cantidadElement = e.target.parentElement.getElementsByClassName("cantidad")[0];
+          cantidadElement.innerText = agregarAlCarrito(producto);
+          actualizarTotales();
+        });
+    });
+  }
+  revisarMensajeVacio();
+  actualizarTotales();
+  actualizarNumeroCarrito();
+}
+
+crearTarjetasProductosCarrito();
+
+/** Actualiza el total de precio y unidades de la página del carrito */
+function actualizarTotales() {
+  const productos = JSON.parse(localStorage.getItem("cervezas"));
+  let cantidad = 0;
+  let precio = 0;
+  if (productos && productos.length > 0) {
+    productos.forEach((producto) => {
+      cantidad += producto.cantidad;
+      precio += producto.precio * producto.cantidad;
+    });
+  }
+  cantidadElement.innerText = cantidad;
+  precioElement.innerText = precio;
+  if(precio === 0) {
+    reiniciarCarrito();
+    revisarMensajeVacio();
+  }
+}
+
+document.getElementById("reiniciar").addEventListener("click", () => {
+  contenedorTarjetas.innerHTML = "";
+  reiniciarCarrito();
+  revisarMensajeVacio();
+});
+
+/** Muestra o esconde el mensaje de que no hay nada en el carrito */
+function revisarMensajeVacio() {
+  const productos = JSON.parse(localStorage.getItem("cervezas"));
+  carritoVacioElement.classList.toggle("escondido", productos);
+  totalesContainer.classList.toggle("escondido", !productos);
+}
